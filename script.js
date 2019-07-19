86414a3d0a8b0bf74d20ed54fe09d0005e767823
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-07-19 17:07:10 +0200

    js prev arrow

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 $( document ).ready(function() {
   // Carousel
+
    // Arrow next del Carousel
   $(".next").click(nextimg);
     // Funzione che richiamiamo nel click
@@ -16,6 +17,23 @@ $( document ).ready(function() {
     } else {
       activeimg.next("img").addClass("active");
     }
-
   }
+
+    // Arrow prev del Carousel
+  $(".prev").click(previmg);
+    // Funzione che richiamiamo nel click
+  function previmg () {
+    // Variabile in cui salviamo lo stato di attivazione
+    var activeimg = $(".carousel img.active");
+    // Al momento del click rimuoviamo la classe active
+    activeimg.removeClass("active");
+
+    // Fissiamo le condizioni
+    if (activeimg.hasClass("first")) {
+      $(".last").addClass("active");
+    } else {
+      activeimg.prev("img").addClass("active");
+    }
+  }
+
 });
