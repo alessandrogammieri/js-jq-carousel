cc0dcb45d5f78fd13e58818d2546afd321f3dd02
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-07-19 16:50:48 +0200

    js next arrow

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-/* Ricreare il Carousel */
 
-console.log("Script collegato");
+$( document ).ready(function() {
+  // Carousel
+   // Arrow next del Carousel
+  $(".next").click(nextimg);
+    // Funzione che richiamiamo nel click
+  function nextimg () {
+    // Variabile in cui salviamo lo stato di attivazione
+    var activeimg = $(".carousel img.active");
+    // Al momento del click rimuoviamo la classe active
+    activeimg.removeClass("active");
+
+    // Fissiamo le condizioni
+    if (activeimg.hasClass("last")) {
+      $(".first").addClass("active");
+    } else {
+      activeimg.next("img").addClass("active");
+    }
+
+  }
+});
